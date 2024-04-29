a3160379513b2677469991e66d6abd109a0777f0
gabitenoriob <gabitenoriob@gmail.com>
2024-04-29 16:56:47 -0300

    provider ok

--- emailService/src/main/java/com/emailService/demo/EmailServiceApplication.java
+++ emailService/src/main/java/com/emailService/demo/EmailServiceApplication.java
@@ -3,6 +3,7 @@ package com.emailService.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class EmailServiceApplication {
 
--- emailService/src/main/java/com/emailService/demo/controllers/EmailSenderController.java
+++ emailService/src/main/java/com/emailService/demo/controllers/EmailSenderController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @RestController
 @RequestMapping("/api/email")
